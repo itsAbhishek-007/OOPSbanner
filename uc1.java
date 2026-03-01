6619168b0a52e251b7d2ae9f67309589ca10e721
Abhishek <abhishek2004maurya@gmail.com>
2026-03-02 03:11:00 +0530

    uc1 created

---
+++ uc1.java
@@ -0,0 +1,15 @@
+/**
+ * uc1
+ *
+ * UC1: Prints the literal text "OOPS" to the console.
+ *
+ * @author Abhishek
+ * @version 1.0
+ */
+public class uc1 {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
