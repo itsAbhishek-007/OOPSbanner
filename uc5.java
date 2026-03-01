61a2da0cee6f2d55f9489dcd931286c2f83cc744
Abhishek <abhishek2004maurya@gmail.com>
2026-03-02 03:45:47 +0530

    uc5 created

---
+++ uc5.java
@@ -0,0 +1,24 @@
+
+public class uc5 {
+
+    public static void main(String[] args) {
+
+        // Array declaration + initialization in ONE statement
+        String[] banner = {
+
+                String.join("  ", " ***** ", " ***** ", " ***** ", " ***** "),
+                String.join("  ", "*     *", "*     *", "*     *", "*     *"),
+                String.join("  ", "*     *", "*     *", "*     *", "*     *"),
+                String.join("  ", "*     *", "*     *", " ***** ", " ***** "),
+                String.join("  ", "*     *", "*     *", "*      ", "      *"),
+                String.join("  ", "*     *", "*     *", "*      ", "      *"),
+                String.join("  ", " ***** ", " ***** ", "*      ", " ***** ")
+
+        };
+
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
